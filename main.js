5fb616f055864089108b5501cad7d9deebb69c65
TTcamui21 <118485908+TTcamui21@users.noreply.github.com>
2023-07-26 18:49:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-<html>
+timerCounter = 0
 
-<head>
-  <meta charset="UTF-8">
-  <title>IDENTIFICAÇÃO DE ESBOÇO</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
+timerCheck =  0
 
-  <script src="https://unpkg.com/ml5@latest/dist/ml5.min.js" type="text/javascript"></script>
-  
-  
+sketch  = quickDrawDataset[randowNamber];
+document.getElementById("sketckName").innerHTML  = "esboço a ser desenhado:" + sketck
+
+
+function checkSketch ()
+{
+
+timerCounter++;
+document.getElementById("tempo").innerHTML = "tempo:" + timerCounter;
+console.log(timerCounter)
+if(timerCounter > 400)
+{
+
+timerCounter  = 0
+timerCheck = "completed"
+}
+
+if(timerCheck =="completed"  ||  answerHolder ==  "set")
+{
+
+timerCheck = "";
+answerHolder = "";
+updateCanvas();
+}
+}
+
     
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
-<body>
 
 
-<script src="main.js"></script>
-</body>
+
